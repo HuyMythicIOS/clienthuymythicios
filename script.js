0ad021d47d944354a4be7574830953004791949c
HuyMythicIOS <huyacc10@gmail.com>
2026-01-10 01:04:22 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,18 +2,10 @@ const openMenu = document.getElementById("openMenu");
 const closeMenu = document.getElementById("closeMenu");
 const menuOverlay = document.getElementById("menuOverlay");
 
-openMenu.onclick = () => {
-  menuOverlay.classList.add("active");
-};
-
-closeMenu.onclick = () => {
-  menuOverlay.classList.remove("active");
-};
-
+openMenu.onclick = () => menuOverlay.classList.add("active");
+closeMenu.onclick = () => menuOverlay.classList.remove("active");
 menuOverlay.onclick = (e) => {
-  if (e.target === menuOverlay) {
-    menuOverlay.classList.remove("active");
-  }
+  if (e.target === menuOverlay) menuOverlay.classList.remove("active");
 };
 
 /* SCROLL REVEAL */
